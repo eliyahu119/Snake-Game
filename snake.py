87e801c8708c8dec9d38cd775e1d9d310154f17d
yaelbarhaim <135954776+yaelbarhaim@users.noreply.github.com>
2023-06-11 19:48:53 +0300

    utils correction

--- snake.py
+++ snake.py
@@ -1,67 +1,36 @@
-
-
-LEFT = "left"
-RIGHT = "right"
-UP = "up"
-DOWN = "down"
+import utils
 
 
 class Snake:
 
-    def __init__(self, started_pos: tuple[int, int]) -> None:
-        x = started_pos[0]
-        y = started_pos[1]
-        self.snake = [(x, y), (x, y-1), (x, y-1)]
-        self.pre_dir = "Up"
-        self.cur_dir = "Up"
+    def __init__(self, started_pos: tuple[int, int], direction: str, length: int = 3) -> None:
+        x, y = started_pos
+        self.snake = [(x, y), (x, y-1), (x, y-2)]
+        self.direction = utils.UP
 
-    def add_length(self, snake: list[tuple[int, int]], cur_dir: str):
+    def add_length(self):
         """
             enlarges the size of the snake by 1
             updates the snake length
         """
-        updated_snake = self.snake
-        x_head = snake.get_head(self.snake)[0]
-        y_head = snake.get_head(self.snake)[1]
-        if cur_dir == 'Left':
-            updated_snake.insert(0, (x_head-1, y_head)
-        elif cur_dir == 'Right':
-            updated_snake.insert(0, (x_head + 1, y_head))
-        elif cur_dir == 'Up':
-            updated_snake.insert(0, (x_head, y_head+1))
-        elif cur_dir == 'Down':
-            updated_snake.insert(0, (x_head, y_head - 1))
-        self.snake = updated_snake
+        head = self.get_head()
+        update = utils.update_location(head, direction=self.direction)
+        self.snake.insert(0, update)
 
     def cut_snake(self, pos: tuple[int, int]) -> None:
         """cut the snake in a certain pos"""
-        #please consider whats happening when the length is 1
-        #remove the
-        pass
+        tail_len = len(self.snake) - self.snake.index(pos)+1
+        for i in range(tail_len):
+            self.snake.pop()
 
     def get_snake_positions(self) -> list[tuple[int, int]]:
         """return the entire list of tuples representing the snake current pos"""
-        return self.snake
+        return list(self.snake)
 
-    def change_dir(self, pre_dir: str, cur_dir: str) -> bool:
-        """checks if direction is allowed
-        return True if it's allowed to turn that way and also updates cur and pre dir"""
-        clicked = game_display.get_key_clicked
-        if clicked == "Left" and pre_dir != "Right":
-            self.pre_dir = self.cur_dir
-            self.cur_dir = clicked
-            return True
-        elif clicked == "Right" and pre_dir != "Left":
-            self.pre_dir = self.cur_dir
-            self.cur_dir = clicked
-            return True
-        elif clicked == "Up" and pre_dir != "Down":
-            self.pre_dir = self.cur_dir
-            self.cur_dir = clicked
-            return True
-        elif clicked == "Down" and pre_dir != "Up":
-            self.pre_dir = self.cur_dir
-            self.cur_dir = clicked
+    def change_dir(self, direction: str) -> bool:
+        """update direction if allowed, returns True if succeeded and False if not"""
+        if direction != utils.opposite_direction(direction):
+            self.direction = direction
             return True
         else:
             return False
@@ -71,16 +40,12 @@ class Snake:
         for _ in range(time_to_move):
             self.__move_snake_one()
 
-    def __move_snake_one(self, snake: list[tuple[int, int]], pre_dir, cur_dir) -> None:
+    def __move_snake_one(self) -> None:
         """
-        if the dir allowed it will move snake to this dir
-        updates the snake
+        move snake to this direction by 1 place - updates the snake
         """
-        if snake.change_dir(self, pre_dir, cur_dir):
-            snake.add_length(self, snake, cur_dir)
-            self.snake.pop()
-        else:
-            return
+        self.add_length()
+        self.snake.pop()
 
     def is_part_of_snake(self, loc: tuple[int, int]) -> bool:
         """checks if a location is part of the snake
@@ -90,12 +55,24 @@ class Snake:
         else:
             return False
 
-    def get_head(self, snake: list[tuple])->tuple[int,int]:
-        """return the position of the head"""
-        return snake[0]
+    def get_head(self) -> tuple[int, int]:
+        """returns the position of the head"""
+        return self.snake[0]
     
-    def check_if_hit_himself()->bool:
-        pass
-    
-    def reset_snake():
-        pass
+    def check_if_hit_itself(self) -> bool:
+        """
+        checks if the snake hit itself
+        returns True if it did or False otherwise
+        """
+        if self.snake.count(self.get_head()) > 1:
+            return True
+        else:
+            return False
+
+    def reset_snake(self, started_pos: tuple[int, int]):
+        """
+        reset the snake completely: location, length and direction
+        """
+        x, y = started_pos
+        self.snake = [(x, y), (x, y - 1), (x, y - 2)]
+        self.direction = utils.UP
